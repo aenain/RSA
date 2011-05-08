bfc6b16d1406e9f6fc25050470425caa7cc1bbf4
Artur Hebda <artur@adtaily.com>
2011-05-08 23:14:44 +0200

    Plik z testami do różnych fragmentów kodu.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
+#include <cstdio>
+#include <cstdlib>
 #define abs(A) ((A) >= 0) ? (A) : -(A)
 using namespace std;
+
+string block_from_char(const char letter) {
+	int ascii = static_cast<int>(letter);
+
+	char * tmp = new char[3];
+	sprintf(tmp, "%d", ascii);
+
+	string block(tmp);
+
+	while (block.length() < 3)
+		block.insert( block.begin(), '0' );
+
+	return block;
+}
 
 int main (int argc, char const* argv[])
 {
@@ -12,5 +28,17 @@ int main (int argc, char const* argv[])
   digit = static_cast<char>(5 + 48);
   digits.insert(digits.begin(), digit);
   cout << digits << endl;
+  
+  char mark = 'a';
+  cout << static_cast<int>(mark) << endl;
+  cout << block_from_char(mark) << endl;
+
+	string napis = "text";
+	napis.erase( napis.begin() );
+	cout << napis << endl;
+
+	string liczba = "068";
+	cout << atoi(liczba.c_str()) << endl;
+	cout << static_cast<char>(68) << endl;
   return 0;
 }
